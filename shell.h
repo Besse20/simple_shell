64263b5eb57b13696f55ba4dcd68328904dc8681
Besufekad_Terefe <besalu20@gmail.com>
2022-08-10 07:12:13 -0700

    final commit

--- shell.h
+++ shell.h
@@ -1,38 +1,39 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
-#include <dirent.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <signal.h>
+#include <stdio.h>
 
 extern char **environ;
 
-void sighandler(int);
-char *validator_getline(void);
-int _memory(char *buff);
-int _memory1(char *buff);
-char **function_strtok(char *buff, int pointer);
-void exit1(char *buffer, char **commands);
-int env1(char *buff, char **commands);
-char *_getenv(char *stri);
-char *_strcpy(char *dest, char *src);
-char *_strdup(char *str);
-char *_strstr(char *haystack, char *needle);
-int _strcmp(char *s1, char *s2);
-char *get_path(char *command);
 char *_strcat(char *dest, char *src);
-int _strlen(char *s);
-char *found_function(char **folder, char *command);
-void function_fork(char *buffer, char **command, char **argv, int count);
-char *_itoa(int number);
-int *_perror(char *argv, char *str, char *command);
-
+int _strlen(const char *s);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t len);
+char *_strdup(char *str);
+int _atoi(char *s);
+int _putchar(char c);
+void _puts(char *str);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int count_input(char *str);
+int count_delims(char *str, char *del);
+char *remove_new_line(char *str);
+void signal_handler(int sig_id);
+void _open_help(void);
+void _print_env(void);
+void _handle_exit(char **u_tokns, char *line);
+int execBuiltInCommands(char **u_tokns, char *line);
+void frees_get_env(char *env_path);
+void frees_tokens(char **tokns);
+int exec(char *cname, char **opts);
+char *_getenv(const char *name);
+char **tokenize(char *str, char *del, int len);
+char *find(char *cname);
 
 #endif
